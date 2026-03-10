1952a7d89f4271467da1dbfdd49bc08ede718c10
olabi.a <olabi.a@coretech.local>
2026-03-10 10:51:32 +0300

    Updated CSS and JavaScript files for responsive design and added confetti effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,36 @@ document.addEventListener("DOMContentLoaded", () => {
         launchSound.play();
       }
 
+      // =========================================
+      // (Confetti) 
+      // =========================================
+      var duration = 2000;
+      var end = Date.now() + duration;
+
+      (function frame() {
+        confetti({
+          particleCount: 5,
+          angle: 60,
+          spread: 55,
+          origin: { x: 0, y: 0.8 },
+          colors: ["#b9a77a", "#054239", "#ffffff"],
+          zIndex: 9999,
+        });
+        confetti({
+          particleCount: 5,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1, y: 0.8 },
+          colors: ["#b9a77a", "#054239", "#ffffff"],
+          zIndex: 9999,
+        });
+
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      })();
+      // =========================================
+
       splashScreen.classList.add("open");
 
       body.classList.remove("splash-active");
